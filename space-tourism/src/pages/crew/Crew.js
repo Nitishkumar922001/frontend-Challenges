77d3210785443b5fc35a18b8b351eace15613ff4
Nitishkumar922001 <miranamnitish@gmail.com>
2023-05-19 07:57:55 +0530

    crew and destination done

--- space-tourism/src/pages/crew/Crew.js
+++ space-tourism/src/pages/crew/Crew.js
@@ -1,10 +1,38 @@
 import NavBar from "../../components/NavBar/NavBar";
 import "./Crew.css";
+import data from '../../data.json'
+import crew1 from '../../assets/crew/image-douglas-hurley.png'
+import crew2 from '../../assets/crew/image-mark-shuttleworth.png'
+import crew3 from '../../assets/crew/image-victor-glover.png'
+import crew4 from '../../assets/crew/image-anousheh-ansari.png'
+import { useState } from "react";
+const crewImg=[crew1,crew2,crew3,crew4];
 function Crew() {
+const [crews,setCrews]=useState(0);
+let {crew}=data;
   return (
     <div className="crew-container">
     <NavBar/>
-      <h1>crew</h1>
+    <h3>03 Meet your crew</h3>
+      <div className="crew-box">
+        <div className="crew-left-part">
+        
+          <h4 className="crew-role">{crew[crews].role}</h4>
+          <h2 className="crew-name">{crew[crews].name}</h2>
+          <p className="crew-bio">{crew[crews].bio}</p>
+          <div className="navigation-btns">
+            <button className={crews===0?"crew-btn crew-active":"crew-btn"} onClick={()=>setCrews(0)}></button>
+            <button className={crews===1?"crew-btn crew-active":"crew-btn"}onClick={()=>setCrews(1)}></button>
+            <button className={crews===2?" crew-btn crew-active":"crew-btn"} onClick={()=>setCrews(2)}></button>
+            <button className={crews===3?"crew-btn crew-active":"crew-btn"} onClick={()=>setCrews(3)}></button>
+  
+          </div>
+        </div>
+        <div className="crew-right-part">
+          <img src={crewImg[crews]} alt="" className="crew-img" />
+          <hr/>
+        </div>
+      </div>
     </div>
   );
 }
--- space-tourism/src/pages/destination/Destination.js
+++ space-tourism/src/pages/destination/Destination.js
@@ -1,10 +1,68 @@
 import NavBar from "../../components/NavBar/NavBar";
 import "./Destination.css";
+import data from "../../data.json";
+import moon from "../../assets/destination/image-moon.png";
+import mars from "../../assets/destination/image-mars.png";
+import europa from "../../assets/destination/image-europa.png";
+import titan from "../../assets/destination/image-titan.png";
+import { useState } from "react";
+const { destinations } = data;
+const image=[moon,mars,europa,titan];
+
 function Destination() {
+  const [planet, setPlanet] = useState(0);
+
   return (
     <div className="destination-container">
-    <NavBar/>
-      <h1>Destination</h1>
+      <NavBar />
+
+      <div className="destination-box">
+        <div className="destination-left-part">
+          <h5>01 pick your destination</h5>
+          <img src={image[planet]} alt={destinations[planet].name} />
+        </div>
+        <div className="destination-right-part">
+          <ul className="des-nav">
+            <li
+              onClick={() => setPlanet(0)}
+              className={planet === 0 ? "planet-active" :'undefined'}
+            >
+              Moon
+            </li>
+            <li
+              onClick={() => setPlanet(1)}
+              className={planet === 1 ? "planet-active" :'undefined'}
+            >
+              mars
+            </li>
+            <li
+              onClick={() => setPlanet(2)}
+              className={planet === 2? "planet-active" :'undefined'}
+            >
+              europa
+            </li>
+            <li
+              onClick={() => setPlanet(3)}
+              className={planet === 3 ? "planet-active" :'undefined'}
+            >
+              titan
+            </li>
+          </ul>
+          <h1 className="planet-name">{destinations[planet].name}</h1>
+          <p>{destinations[planet].description}</p>
+          <hr />
+          <div className="planet-footer">
+            <div className="distance">
+              <p>AVG. DISTANCE</p>
+              <h5>{destinations[planet].distance}</h5>
+            </div>
+            <div className="travel">
+              <p>EST.TRAVEL TIME</p>
+              <h5>{destinations[planet].travel}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
